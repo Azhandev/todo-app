182e6eabbeafdd2e2a917aa7f58914d108df9c4c
Muhammad Azhan <156644698+Azhandev@users.noreply.github.com>
2024-07-29 03:47:43 +0500

    index

---
+++ app.js
@@ -0,0 +1,48 @@
+
+const inputs = document.querySelector('#todo')
+const ul = document.querySelector('#ul')
+let todoApp = []
+
+function addtodo() {
+    todoApp.push(inputs.value)
+    ul.innerHTML = ``
+    console.log(todoApp);
+    inputs.value = '';
+    // deleteRow()
+
+    for (let i = 0; i < todoApp.length; i++) {
+
+        ul.innerHTML += `<li>
+        ${todoApp[i]} 
+        <button class="buttons" onclick="deleted(${i})">Remove</button>
+        <button class="buttons" onclick="edit(${i})">Change</button>
+        </li>`;
+    }
+
+}
+function deleted(indexNo) {
+    console.log(`delete todo`, indexNo);
+    todoApp.splice(indexNo, 1);
+    deleteRow()
+}
+
+function deleteRow() {
+    ul.innerHTML = ``
+    for (let i = 0; i < todoApp.length; i++) {
+        ul.innerHTML += `<li>
+        ${todoApp[i]} 
+        <button class="buttons" onclick="deleted(${i})">Remove</button>
+        <button class="buttons" onclick="edit(${i})">Change</button>
+        </li>`;
+    }
+
+}
+
+function edit(indexNo) {
+    let editValue = prompt('Enter Ubbdated Value', todoApp[indexNo])
+
+    console.log(`edit todo`, indexNo);
+    todoApp.splice(indexNo, 1, editValue);
+    deleteRow()
+}
+
